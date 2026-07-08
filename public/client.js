99df34b5598123ba3d612ad650b0773df6af49d2
lsq630 <liaosiqi666@outlook.com>
2026-07-08 11:46:27 +0800

    修改 Render 域名

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const socket = io();
+const socket = io('https://xian-io.onrender.com');
 
 let players = {};
 let monsters = {};
